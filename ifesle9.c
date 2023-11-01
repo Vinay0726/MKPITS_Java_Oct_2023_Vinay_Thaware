81289089b8037bfbc1dace0c2efa69db3b72317b
Vinay0726 <vinaythaware143@gmail.com>
2023-11-01 12:54:39 +0530

    updated code

--- ifesle9.c
+++ ifesle9.c
@@ -1,38 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
+
 main()
 {
-	float unit,bill;
-	printf("Enter a units consume\n");
-	scanf("%f", &unit);
+	float unit;
+	printf("Enter the Electric Unit :-");
+	scanf("%f",&unit);
 	
 	if(unit<100)
-	{
-		bill=unit*1;
-		printf("Electricity bill : %f",bill);
+	printf("Total Bill :-%f ",unit*1.0);
+	else if(unit>=100 && unit<200)
+	printf("Total Bill :-%f ",unit*1.25);
+	else if(unit>=200 && unit<300)
+	printf("Total Bill :-%f ",unit*1.50);
+	else if(unit>=300 && unit<400)
+	printf("Total Bill :-%f ",unit*1.75);
+	else
+	printf("Total Bill :-%f ",unit*2);
 	
-	}
-	else if(unit>100 && unit<200)
-	{
-		bill=100*1+(unit-100)*1.25;
-		printf("Electricity bill : %f",bill);
-	}
-		else if(unit>200 && unit<300)
-	{
-	    bill=100*1+100*1.25+(unit-200)*1.50;
-	    printf("Electricity bill : %f",bill);
-	}
-		else if(unit>300 && unit<400)
-	{
-	     bill=unit*1+100*1.25+100*1.50+(unit-300)*1.75;
-	     printf("Electricity bill : %f",bill);
-	
-	}
-		else if(unit>=400)
-	{
-		bill=unit*1+100*1.25+100*1.50+100*1.75+(unit-100)*2;
-		printf("Electricity bill : %f",bill);
-	
-	}
-	
+	getch();
 }
-
