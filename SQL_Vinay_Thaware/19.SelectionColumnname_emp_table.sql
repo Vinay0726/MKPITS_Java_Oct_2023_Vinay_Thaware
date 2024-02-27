c35e61e064d7f8333f7d14aa791b58cdd6d677f2
Vinay0726 <vinaythaware143@gmail.com>
2024-02-27 20:26:32 +0530

    new sql file added

---
+++ SQL_Vinay_Thaware/19.SelectionColumnname_emp_table.sql
@@ -0,0 +1,8 @@
+create database emp;
+use emp;
+create table emp_table(Emp_Id int primary key not null,Emp_Name varchar(255),Emp_City varchar(255),Salary float);
+insert into  emp_table values (1,"Vinay","Nagpur",30000);
+insert into  emp_table values (2,"Raj","Nagpur",25000),(3,"Ashiq","Nagpur",40000),(4,"Sanket","Amravati",35000);
+select * from emp_table;
+select Emp_Name,Salary from emp_table;
+select * from emp_table where salary>25000;
